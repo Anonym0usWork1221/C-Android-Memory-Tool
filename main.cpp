a7373487219f6c906015854d61e00ecf2921cdd1
Abdul Moez <72302783+Anonym0usWork1221@users.noreply.github.com>
2023-06-11 01:37:13 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 #include "MemoryTools.h"
 
-int main(int argc, char *argv[]) {
-    initXMemoryTools(
-        /*process name*/
-        "com.tencent.ig",
-        /*mode root/no root*/
-        MODE_ROOT
-        /*MODE_NO_ROOT - for no root*/
-    );
-
-/*
-    TYPE_DWORD,
-    TYPE_FLOAT,
-    TYPE_DOUBLE,
-    TYPE_WORD,
-    TYPE_BYTE,
-    TYPE_QWORD,
-*/
-
-    SetSearchRange(ALL);
-    MemorySearch("1", TYPE_BYTE);
-    MemoryOffset("2", 2,/*offset*/ TYPE_BYTE);
-    MemoryWrite("0", 2,/*offset*/ TYPE_BYTE);
-    ClearResults();
+int main(int argc, char* argv[]) {
+    /* 1. Package Name of the game
+    2. ROOT_MODE (tells you are using rooted device (not reooted device not implimented yet))
+    */
+    MemoryTools memoryTools("com.tencent.ig", MODE_ROOT);
     
+    // Initialize the search range to global (check other ranges in header file of memory tool)
+    memoryTools.SetSearchRange(ALL);
+    // Search for 1 in whole memory of type byte
+    memoryTools.MemorySearch("1", TYPE_BYTE);
+    // This is another usage if you want to write a value at given offset you can use below line else above line will write all the data in memory 
+    memoryTools.MemoryOffset("2", 2, /*offset*/ TYPE_BYTE);
+    // Finally write the value to the memory
+    memoryTools.MemoryWrite("0", 2, /*offset*/ TYPE_BYTE);
+    // clear results function clean all the offsets/values from memory
+    memoryTools.ClearResults();
+
     return 0;
 }
